1a44da77c0069961da89b7862e50115cd5d8d2b5
kishorchouhan <devkishorbwr@gmail.com>
2020-03-21 01:06:10 +0530

    Updates

---
+++ precache-manifest.791baf29665adc70773355ba31b86450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fee7e820e3c54b1175df5bf130072a3",
+    "url": "/itune-search-app-react/index.html"
+  },
+  {
+    "revision": "40cc254f6cd2230be831",
+    "url": "/itune-search-app-react/static/css/main.a6b90fc2.chunk.css"
+  },
+  {
+    "revision": "c8c466e615f4dc6ae6c0",
+    "url": "/itune-search-app-react/static/js/2.bb44baeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/itune-search-app-react/static/js/2.bb44baeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cc254f6cd2230be831",
+    "url": "/itune-search-app-react/static/js/main.565b6b61.chunk.js"
+  },
+  {
+    "revision": "d11d2b254d227fee6417",
+    "url": "/itune-search-app-react/static/js/runtime-main.cd1b7a0f.js"
+  },
+  {
+    "revision": "67b3520b61a8f055b20b22fb920e5c17",
+    "url": "/itune-search-app-react/static/media/pause-play-button.67b3520b.svg"
+  }
+]);
